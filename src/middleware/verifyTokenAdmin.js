29f391ab65ed1d98662a79b2a87b20ee815b7445
jovivaspo <jovivaspo@gmail.com>
2022-06-27 18:34:11 +0200

    verify token edited

--- src/middleware/verifyTokenAdmin.js
+++ src/middleware/verifyTokenAdmin.js
@@ -2,45 +2,39 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 const Admin = require('../models/Admin')
 
-const verifyTokenAdmin = async ( req, res, next) => {
-    try{
+const verifyTokenAdmin = async (req, res, next) => {
+    try {
         const authorization = req.get('Authorization')
         let token = ""
-        if(authorization && authorization.toLowerCase().startsWith("bearer")){
+        if (authorization && authorization.toLowerCase().startsWith("bearer")) {
             token = authorization.substring(7)
         }
-        const decodedToken = jwt.verify(token,config.KEY)
+         jwt.verify(token, config.KEY, async (err, decodedToken) => {
+            if (err) {
+                res.status(401)
+                const error = new Error(err)
+                return next(error)
+            }
+            const admin = await Admin.findOne({ email: decodedToken.email })
+
+
+            if (!admin) {
+                res.status(401)
+                const error = new Error("Permiso denegado")
+                return next(error)
+            }
+    
+            console.log("Token valido")
+            next()
+        })
+
       
-        if(!token || !decodedToken.id){
-            res.status(401)
-            const error = new Error("Token invalido")
-           return next(error)
-        }
-
-        if(Date.now() >= decodedToken.exp * 1000){
-            res.status(401)
-            const error = new Error("Token expirado")
-            return next(error)
-        }
-
-        //console.log(decodedToken)
-    
-        const admin = await Admin.findOne({email:decodedToken.email})
-    
-       
-        if(!admin){
-            res.status(401)
-            const error = new Error("Permiso denegado")
-           return next(error)
-        }
-        
-        console.log("Token valido")
-        next()
-    }catch(error){
+    } catch (error) {
+        console.log(error)
         res.status(401)
         return next(error)
     }
-  
+
 }
 
 module.exports = verifyTokenAdmin
--- src/middleware/verifyTokenUser.js
+++ src/middleware/verifyTokenUser.js
@@ -2,49 +2,45 @@ const jwt = require('jsonwebtoken')
 const config = require('../config')
 const User = require('../models/User')
 
-const verifyTokenUser = async ( req, res, next) => {
-    try{
+const verifyTokenUser = async (req, res, next) => {
+    try {
         const authorization = req.get('Authorization')
         let token = ""
-        if(authorization && authorization.toLowerCase().startsWith("bearer")){
+        if (authorization && authorization.toLowerCase().startsWith("bearer")) {
             token = authorization.substring(7)
         }
-        const decodedToken = jwt.verify(token,config.KEY)
-       
-        if(!token || !decodedToken.id){
-            res.status(401)
-            const error = new Error("Token no existe o es inválido")
-           return next(error)
-        }
 
-        if(Date.now() >= decodedToken.exp * 1000){
-            res.status(401)
-            const error = new Error("Token expirado")
-            return next(error)
-        }
+        jwt.verify(token, config.KEY, async (err, decodedToken) => {
+            if (err) {
+                res.status(401)
+                const error = new Error(err)
+                return next(error)
+            }
+            const user = await User.findOne({ email: decodedToken.email })
+
+            if (!user) {
+                res.status(401)
+                const error = new Error("Permiso denegado")
+                return next(error)
+            }
+
+            if (user.verified === "Not verified") {
+                res.status(401)
+                const error = new Error("Debe confirmar su cuenta")
+                return next(error)
+            }
     
-        const user = await User.findOne({email:decodedToken.email})
+            console.log("Token valido")
+            next()
 
-        if(!user){
-            res.status(401)
-            const error = new Error("Permiso denegado")
-           return next(error)
-        }
 
-        if(user.verified === "Not verified"){
-            res.status(401)
-            const error = new Error("Debe confirmar su cuenta")
-            return next(error)
-        }
-        
-        console.log("Token valido")
-        next()
-        
-    }catch(error){
+        })
+
+    } catch (error) {
         res.status(401)
         return next(error)
     }
-  
+
 }
 
 module.exports = verifyTokenUser
